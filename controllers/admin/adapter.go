6745ddb9aabd3a87e8be221f5f46486a88978471
Beatrice950201 <beatrice950201@163.com>
2020-07-03 14:36:47 +0800

    提交栏目转化基础

---
+++ controllers/admin/adapter.go
@@ -0,0 +1,9 @@
+package admin
+
+/** 蜘蛛池栏目转换工具 **/
+type Adapter struct{ Main }
+
+// @router /adapter/index [get,post]
+func (c *Adapter) Index() {
+
+}
--- routers/router.go
+++ routers/router.go
@@ -33,6 +33,7 @@ func init() {
 			&admin.Models{}, &admin.Disguise{}, &admin.Template{},
 			&admin.Class{}, &admin.Article{}, &admin.Prefix{}, &admin.Match{},
 			&admin.Arachnid{}, &admin.Keyword{}, &admin.Indexes{}, &admin.Statistics{},
+			&admin.Adapter{},
 		)),
 	)
 }
