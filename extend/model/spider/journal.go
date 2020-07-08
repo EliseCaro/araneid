5f24058acd6ebe91ec6b3bcc814617cb9ad6b321
Beatrice950201 <beatrice950201@163.com>
2020-07-08 17:56:43 +0800

    更新日志记录

--- extend/model/spider/journal.go
+++ extend/model/spider/journal.go
@@ -7,7 +7,7 @@ type Journal struct {
 	Urls        string    `orm:"column(urls);size(200);type(char);default();description(地址)" json:"urls" form:"urls"`
 	Usage       int       `orm:"column(usage);type(int);default(0);description(访问次数)" json:"usage" form:"usage"`
 	Domain      string    `orm:"column(domain);size(32);type(char);default();description(域名)" json:"domain" form:"domain"`
-	SpiderIp    int64     `orm:"column(spider_ip);default(0);description(访问IP)" json:"spider_ip" form:"spider_ip"`
+	SpiderIp    string    `orm:"column(spider_ip);size(32);type(char);default();description(访问IP)" json:"spider_ip" form:"spider_ip"`
 	SpiderName  string    `orm:"column(spider_name);size(32);type(char);default();description(蜘蛛标识)" json:"spider_name" form:"spider_name"`
 	SpiderTitle string    `orm:"column(spider_title);size(32);type(char);default();description(蜘蛛标题)" json:"spider_title" form:"spider_title"`
 	CreateTime  time.Time `orm:"auto_now_add;type(datetime);description(创建时间)" json:"create_time"`
--- extend/service/journal.go
+++ extend/service/journal.go
@@ -3,20 +3,44 @@ package service
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/orm"
 	_func "github.com/beatrice950201/araneid/extend/func"
+	"github.com/beatrice950201/araneid/extend/model/spider"
 	"net/http"
 	"strings"
 )
 
 type DefaultJournalService struct{}
 
-/** 获取一条数 **/
+/** 根据URL获取一条数据 **/
+func (service *DefaultJournalService) One(urls string) spider.Journal {
+	var maps spider.Journal
+	_ = orm.NewOrm().QueryTable(new(spider.Journal)).Filter("urls", urls).One(&maps)
+	return maps
+}
+
+/** 初始化记录接口 **/
 func (service *DefaultJournalService) HandleInstantiation(ctx *context.Context) {
-	beego.Warn(ctx.Input.Domain())
-	beego.Warn(service.GetURL(ctx.Request))
-	beego.Warn(ctx.Input.IP())
-	beego.Warn(ctx.Input.UserAgent())
-	service.UserAgent()
+	if n, t := service.UserAgent(ctx.Input.UserAgent()); n != "" {
+		item := spider.Journal{
+			SpiderName:  n,
+			SpiderTitle: t,
+			Urls:        service.GetURL(ctx.Request),
+			Domain:      ctx.Input.Domain(),
+			SpiderIp:    ctx.Input.IP(),
+		}
+		var message error
+		if one := service.One(item.Urls); one.Id > 0 {
+			one.Usage += 1
+			_, message = orm.NewOrm().Update(&one)
+		} else {
+			_, message = orm.NewOrm().Insert(&item)
+		}
+		if message != nil {
+			logs.Error("蜘蛛访问记录失败；失败原因：%s", message.Error())
+		}
+	}
 }
 
 /** 获取完整url **/
@@ -29,7 +53,14 @@ func (service *DefaultJournalService) GetURL(r *http.Request) (Url string) {
 }
 
 /** 获取蜘蛛标识 **/
-func (service *DefaultJournalService) UserAgent() {
-	class := _func.ParseAttrConfigArray(beego.AppConfig.String("system_spider_class"))
-	beego.Warn(class)
+func (service *DefaultJournalService) UserAgent(agent string) (n, t string) {
+	class := _func.ParseAttrConfigMap(beego.AppConfig.String("system_spider_class"))
+	for index, item := range class {
+		if strings.Index(agent, index) > 0 {
+			n = index
+			t = item
+			break
+		}
+	}
+	return n, t
 }
