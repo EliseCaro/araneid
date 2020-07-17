06aa00354b2d2f5d78777abda81f8bd9e5c667be
Beatrice950201 <beatrice950201@163.com>
2020-07-17 21:58:04 +0800

    修复词典采集器

--- static/admin/pages/socket_begin.js
+++ static/admin/pages/socket_begin.js
@@ -17,16 +17,17 @@ const socket_begin = {
         assemble:function (data) {
             let count  = parseInt(socket_begin.option.count_box.html());
             let length = socket_begin.option.notifications.children("li").length;
+            let className = data.id ? "open_iframe" : "";
             let html  = "<li class='animated bounce'>";
-                html += '<a class="text-dark media py-2 open_iframe" data-area="380px,240px" href="/admin/inform/check?:id='+ data.id +'&:popup=1">'
+                html += '<a class="text-dark media py-2 '+ className +'" data-area="380px,240px" href="/admin/inform/check?:id='+ data.id +'&:popup=1">'
                 html += '<div class="mr-2 ml-3"><i class="fa fa-fw fa-eye text-amethyst-dark"></i></div>'
                 html += '<div class="media-body pr-2">'
                 html += '<div class="font-weight-normal font-size-sm">'+ data.context +'...</div>'
                 html += '<small class="text-muted">'+ data.string_time +'</small>';
                 html += '</div></a></li>';
-          if (length >= 5) {
+            if (length >= 5) {
               socket_begin.option.notifications.children("li")[socket_begin.option.notifications.children("li").length-1].remove();
-          }
+            }
             socket_begin.option.notifications.prepend(html)
             socket_begin.option.count_box.html(count+1)
         }
