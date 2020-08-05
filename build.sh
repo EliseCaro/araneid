913bd5f2b793382b8a87f69911255bc678699b36
Beatrice950201 <beatrice950201@163.com>
2020-08-06 01:43:22 +0800

    修复编码转换；已经修复打包文件

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd $GOPATH/src/github.com/beatrice950201/araneid/
+ls
+rm -rf $GOPATH/src/github.com/qiniu/iconv
 go get github.com/beego/bee
+go get github.com/qiniu/iconv
 bee pack -be GOOS=linux
